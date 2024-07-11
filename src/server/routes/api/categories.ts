fd1438f92fd69aab5e4b35c63ec24db21f134ed5
Nate Mills <natemills018@gmail.com>
2024-07-11 18:12:36 -0400

    Testing my routes, added register route for users

--- src/server/routes/api/categories.ts
+++ src/server/routes/api/categories.ts
@@ -1,5 +1,19 @@
 import {Router} from 'express'
+import db from '../../db';
 
 const router = Router();
+
+router.get('/' , async (req, res) => {
+    try {
+        const books = await db.categories.getAll()
+        res.json(books)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server Error, cant retrieve books', error})
+    }
+
+   
+})
+
 
 export default router;
