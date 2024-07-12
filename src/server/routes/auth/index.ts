9e7ab50a655acbff684a06aefd010c8589ab62ba
Nate Mills <natemills018@gmail.com>
2024-07-11 20:42:08 -0400

    Troubleshooting front-end

--- src/server/routes/auth/index.ts
+++ src/server/routes/auth/index.ts
@@ -1,9 +1,14 @@
 import {Router} from 'express'
 import loginRouter from './login';
 import registerRouter from './register';
+import tokenCheck from '../../middlewares/tokenCheck';
 
 const router = Router();
 
+
+router.get('/verify', tokenCheck, (req, res) => {
+    res.json({message: 'Nice!'})
+})
 router.use('/login', loginRouter)
 router.use('/register', registerRouter);
 
